edbc974bc3dd97a24752a7201be62ea754dc9dbf
Marco Nevigato <marco.nevigato@gmail.com>
2025-03-12 19:52:56 +0000

    Subir ejercicios de lección 1

---
+++ L01/ejercicio.py
@@ -0,0 +1,3 @@
+print("Hola bienbenido a python Marco")
+print(8 + 7)  # Suma
+print ("Hola quiero que te diviertas mucho")
---
+++ L01/print.py
@@ -0,0 +1,9 @@
+print("¡Hola! Soy tu MacBook.")  
+print("te quiero Marco.")
+
+
+
+print(8 + 7)  # Suma
+print(10 - 67)  # Resta
+print(4 * 3)  # Multiplicación
+print(1 / 0)  # División

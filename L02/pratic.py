8253409637709980727952666de7bda89fa1a099
Marco Nevigato <marco.nevigato@gmail.com>
2025-03-16 19:06:41 +0000

    ejercicios de L02

---
+++ L02/pratic.py
@@ -0,0 +1,6 @@
+nombre = "co"
+edad = 18
+
+
+print("Hola, me llamo", nombre)
+print("Tengo", edad, "años")
---
+++ L02/ro.py
@@ -0,0 +1,16 @@
+nombre = "co"
+edad = 7
+color = "el negro"
+mo = "macbook pro"
+
+print("Hola, me llamo", nombre)
+print("Tengo", edad, "años")
+print("Mi color favorito es", color)
+print("me gusta mi", mo)
+
+print(f"Hola, me llamo {nombre}")
+print(f"Tengo {edad} años")
+print(f"Mi color favorito es {color}")
+print(f"me gusta mi {mo}")
+
+print(nombre * 20)
